db305a3c1b008f0075d697aa9e37f76d85ab9ab2
lmcorazolla <mateus.corazolla@gmail.com>
2022-03-17 17:51:20 -0300

    quebrando cabeça

--- src/DomingoDeManha.java
+++ src/DomingoDeManha.java
@@ -6,14 +6,22 @@ public class DomingoDeManha {
     public static void main(String[] args) throws IOException {
         Scanner leitor = new Scanner(System.in);
 
-        String[] relogio = leitor.nextLine().split(":");
-        int hora = Integer.parseInt((relogio[0]));
-        int min = Integer.parseInt((relogio[1]));
-        switch (hora) {
-            case(7): System.out.println("Atraso maximo: " + min);
-            case(8): System.out.println("Atraso maximo: " + (60 + min));
-            case(9): System.out.println("Atraso maximo: " + (120 + min));
-            default: System.out.println("Atraso maximo: 0");
+        while (true) {
+            String[] relogio = leitor.nextLine().split(":");
+//            System.out.println(relogio[0]+relogio[1]);
+            int hora = Integer.parseInt(relogio[0]);
+            int min = Integer.parseInt(relogio[1]);
+
+                if (hora==7)
+                    System.out.println("Atraso maximo: " + min);
+                if(hora==8)
+                    System.out.println("Atraso maximo: " + (60 + min));
+                if(hora==9)
+                    System.out.println("Atraso maximo: " + (120 + min));
+                else if (hora!=7 && hora!=8 && hora !=9)
+                    System.out.println("Atraso maximo: 0");
+
         }
 
-    }}
+    }
+}
